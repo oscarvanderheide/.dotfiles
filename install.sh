e22c5c8b4aaf98f32cb60f4c875f8e9a4624e346
oscar <oscarvanderheide@gmail.com>
2025-04-15 09:49:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,11 @@ if test ! $(which brew); then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-
-
 # Update Homebrew recipes
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
+# brew tap homebrew/bundle
 brew bundle
 
 # Not sure if I want to keep using Mackup
@@ -29,20 +27,27 @@ brew bundle
 source .macos
 
 # Set git options (from https://spin.atomicobject.com/2020/05/05/git-configurations-default/)
+git config --global user.name "oscar"
+git config --global user.email "oscarvanderheide@gmail.com"
 git config --global pull.rebase true
 git config --global fetch.prune true
 git config --global diff.colorMoved zebra	
 git config --global core.excludesfile ~/.config/git/.gitignore_global
 
+# Install juliaup using defaults 
+curl -fsSL https://install.julialang.org | sh -s -- --yes
+
 # Use stow to symlink dotfiles
-# stow -v -t ~ fish
-# stow -v -t ~ git
-# stow -v -t ~ wezterm
-# stow -v -t ~ tmux
-# stow -v -t ~ bat
-# stow -v -t ~ nvim
-# stow -v -t ~ zsh
+stow -v -t ~ bat
+stow -v -t ~ git
+stow -v -t ~ kmonad
+stow -v -t ~ nvim
+stow -v -t ~ tmux
+stow -v -t ~ wezterm
+stow -v -t ~ yazi
+stow -v -t ~ zsh
 
-
+# Make symbolic link in home to the .zshrc file 
+ln -s /Users/$USER/.config/zsh/.zshrc .zshrc 
 # Manually install the following tools
 # Dropover
