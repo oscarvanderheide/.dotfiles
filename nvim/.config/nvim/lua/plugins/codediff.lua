3ee02d4b0b475564601f7a92a675bde45e5064da
oscar <oscarvanderheide@gmail.com>
2026-01-31 20:38:59 +0100

    Nicer colors for CodeDiff

--- nvim/.config/nvim/lua/plugins/codediff.lua
+++ nvim/.config/nvim/lua/plugins/codediff.lua
@@ -4,4 +4,14 @@ vim.pack.add({ "https://github.com/esmuellert/codediff.nvim" })
 require("codediff").setup({
 	dependencies = { "MunifTanjim/nui.nvim" },
 	cmd = "CodeDiff",
+	-- By default, the inserts are blueish. I like these colors betters.
+	-- Also, by default, the characters have slightly different colors than the lines
+	-- I prefer them to be the same.
+	highlights = {
+		line_insert = "#384534",
+		char_insert = "#384534",
+		line_delete = "#4C3136",
+		char_delete = "#4C3136",
+		char_brightness = 1.6,
+	},
 })
