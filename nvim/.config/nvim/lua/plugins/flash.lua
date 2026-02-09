477b2048e31c05903a6c640a5f52eab3cbea3041
oscar <oscarvanderheide@gmail.com>
2026-02-09 10:22:32 +0100

    Tried smart-motion but back to flash for now

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -25,3 +25,46 @@ end, { desc = "Flash" })
 -- vim.keymap.set("c", "<c-s>", function()
 -- 	flash.toggle()
 -- end, { desc = "Toggle Flash search" })
+--
+-- vim.pack.add({ "https://github.com/FluxxField/smart-motion.nvim" })
+--
+-- require("smart-motion").setup({
+-- 	presets = {
+-- 		words = {
+-- 			w = { map = false }, -- Register but don't map w
+-- 			b = { map = false }, -- Register but don't map b
+-- 			e = false,
+-- 			ge = false,
+-- 		},
+-- 		search = true, -- Disable search to free up s/S
+-- 		misc = true,
+-- 	},
+-- })
+--
+-- -- Now register custom motions for s and S that use the w and b functionality
+-- local smart_motion = require("smart-motion")
+--
+-- local motion_w = smart_motion.motions.get_by_key("w")
+-- local motion_b = smart_motion.motions.get_by_key("b")
+--
+-- if motion_w then
+-- 	smart_motion.motions.register(
+-- 		"s_motion",
+-- 		vim.tbl_extend("force", motion_w, {
+-- 			trigger_key = "s",
+-- 			map = true,
+-- 			modes = { "n", "v", "o" },
+-- 		})
+-- 	)
+-- end
+--
+-- if motion_b then
+-- 	smart_motion.motions.register(
+-- 		"S_motion",
+-- 		vim.tbl_extend("force", motion_b, {
+-- 			trigger_key = "S",
+-- 			map = true,
+-- 			modes = { "n", "v", "o" },
+-- 		})
+-- 	)
+-- end
