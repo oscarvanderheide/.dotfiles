98985bd173c7862e50615b00d0f75c54d5c320c5
oscar <oscarvanderheide@gmail.com>
2026-03-16 09:45:27 +0100

    Set colorcolumn and textwidth for markdown files

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -98,3 +98,20 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.expandtab = true
 	end,
 })
+
+-- Set colorcolumn and textwidth to 80 for markdown files, and reset to 120 when leaving
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	pattern = { "*.md" },
+	callback = function()
+		vim.opt.colorcolumn = "80"
+		vim.opt.textwidth = 80
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+	pattern = { "*.md" },
+	callback = function()
+		vim.opt.colorcolumn = "120"
+		vim.opt.textwidth = 120
+	end,
+})
